68352a679bdcb73612095ed4c444dd045344e9b6
KAYITARES <cyntkayitare@gmail.com>
2023-08-03 14:40:52 +0200

    add

--- js/login.js
+++ js/login.js
@@ -1,1 +1,17 @@
+let email=document.getElementById("email")
+let password=document.getElementById("password")
+let form=document.querySelector("form")
 
+let users=JSON.parse(localStorage.getItem("users")) || []
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+const userExist=users.find((user)=>user.email==email.value)
+if(userExist && userExist.password==password.value){
+    window.location.href="../admin.html"
+}else if(userExist && userExist.password!=password.value){
+alert("Wrong Password")
+}else{
+    alert("user not exist! signup first")
+    window.location.href="../signup.html"
+}
+})

f78745d0434895f433a0d44027c00504571abf63
KAYITARES <cyntkayitare@gmail.com>
2023-08-03 12:18:44 +0200

    add

---
+++ js/signup.js
@@ -0,0 +1,25 @@
+let username=document.getElementById("username")
+let email=document.getElementById("email")
+let password=document.getElementById("password")
+let form=document.querySelector("form")
+
+let users=JSON.parse(localStorage.getItem("users")) || []
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+let user={
+    name:username.value,
+    email:email.value,
+    password:password.value
+}
+if(users.find((userz)=>userz.email==email.value)){
+    alert("user already exist")
+}else{
+    users.push(user);
+    const stringify=JSON.stringify(users)
+    localStorage.setItem("users",stringify)
+    console.log(localStorage)
+}
+
+
+})
